0a69cbe07e0e81c5e30c4e8e4d2b9e8202f1d9be
Brook Tarekegn <brooksideas@gmail.com>
2024-04-15 05:08:04 +0200

    deploy: Production URL set

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,3 @@
 export const urls = {
-  BASE_API:
-    "https://r6qlxlk2ch.execute-api.us-east-1.amazonaws.com/Test-House-Api/fec",
+  BASE_API: "https://r6qlxlk2ch.execute-api.us-east-1.amazonaws.com/fec-api/fec",
 };
