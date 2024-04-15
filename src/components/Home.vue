22397627574e55c9fd1ed3df6b1d81d5957854ca
Brook Tarekegn <brooksideas@gmail.com>
2024-04-16 01:43:40 +0200

    code: Home Progress Bar integration

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,14 +22,41 @@ export default {
     // Display view
     const displayView = ref(views.HOUSE);
 
+    // Loading State
+    const isLoading = ref(false);
+
+    // Progress Value
+    const progressValue = ref(15);
+
     // Listen for view render Event
     bus.on("renderEvt", (renderEvt) => {
       displayView.value = renderEvt;
     });
 
+    // Listen to the Loading state start and stop from Map and chart Component
+    bus.on("triggerLoadingStateEvt", (loadingState) => {
+      isLoading.value = loadingState;
+      progressValue.value = 15;
+      // When the value is true we would like to show the progress values
+      if (loadingState) {
+        handleLoadingState();
+      } else {
+        // Reset the progress Value
+        progressValue.value = 15;
+      }
+    });
+
     const title = ref("Electoral Visualization");
     const description = ref("House Data Visualization");
     const isSmallScreen = ref(window.innerWidth < 1400);
+
+    onMounted(() => {
+      window.addEventListener("resize", handleResize);
+    });
+
+    onBeforeUnmount(() => {
+      window.removeEventListener("resize", handleResize);
+    });
 
     const handleResize = () => {
       isSmallScreen.value = window.innerWidth < 1400;
@@ -45,14 +72,25 @@ export default {
           return "Total Votes count Visualization";
       }
     });
+    const handleLoadingState = () => {
+      const interval = 2000; // Interval between increments in milliseconds
 
-    onMounted(() => {
-      window.addEventListener("resize", handleResize);
-    });
-
-    onBeforeUnmount(() => {
-      window.removeEventListener("resize", handleResize);
-    });
+      const timer = setInterval(() => {
+        if (progressValue.value < 90) {
+          if (progressValue.value === 30) {
+            progressValue.value = 60;
+          } else if (progressValue.value === 60) {
+            progressValue.value = 75;
+          } else if (progressValue.value === 75) {
+            progressValue.value = 90;
+          } else {
+            progressValue.value += 15;
+          }
+        } else {
+          clearInterval(timer);
+        }
+      }, interval);
+    };
 
     return {
       views,
@@ -60,6 +98,8 @@ export default {
       description,
       displayView,
       isSmallScreen,
+      isLoading,
+      progressValue,
       handleDescriptionRender,
     };
   },
@@ -68,6 +108,37 @@ export default {
 
 <template>
   <div id="app">
+    <!-- Progress Bar for data loading -->
+    <div
+      v-if="isLoading"
+      class="bg-white rounded-xl shadow-sm overflow-hidden p-1"
+    >
+      <div class="relative h-6 flex items-center justify-center">
+        <div
+          class="progress absolute top-0 bottom-0 left-0 rounded-lg"
+          :class="{
+            'w-[15%] bg-violet-200': progressValue == 15,
+            'w-[30%] bg-orange-200': progressValue == 30,
+            'w-[60%] bg-sky-500': progressValue == 60,
+            'w-[75%] bg-teal-200': progressValue == 75,
+            'w-[90%] bg-green-200': progressValue == 90,
+          }"
+        ></div>
+        <div
+          class="relative font-medium text-sm"
+          :class="{
+            'text-violet-900': progressValue == 15,
+            'text-orange-900': progressValue == 30,
+            'text-sky-900': progressValue == 60,
+            'text-teal-900': progressValue == 75,
+            'text-green-900': progressValue == 90,
+          }"
+        >
+          {{ progressValue }}%
+        </div>
+      </div>
+    </div>
+
     <div class="grid grid-cols-12 gap-4 content-start items-center">
       <div
         class="flex justify-center w-full"
@@ -78,6 +149,8 @@ export default {
       >
         <display-options-list />
       </div>
+
+      <!-- The Body that renders the Maps and Chart -->
       <div
         :class="{ 'col-span-8': !isSmallScreen, 'col-span-12': isSmallScreen }"
       >
@@ -97,8 +170,28 @@ export default {
   </div>
 </template>
 
+
 <style>
 #app {
   max-width: 1400px !important;
+}
+
+.progress {
+  animation: progress 1s infinite linear;
+}
+
+.left-right {
+  transform-origin: 0% 50%;
+}
+@keyframes progress {
+  0% {
+    transform: translateX(0) scaleX(0);
+  }
+  40% {
+    transform: translateX(0) scaleX(0.4);
+  }
+  100% {
+    transform: translateX(100%) scaleX(0.5);
+  }
 }
 </style>
